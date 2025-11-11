c62713b2cc47f8ddde76889dc3a09061d930958b
Kundan Gupta <kundankumarsobh@gmail.com>
2025-11-11 18:53:12 +0530

    More about arrays

---
+++ 10. ARRAYS (Part II)/MaxSubArraySum2.java
@@ -0,0 +1,29 @@
+public class MaxSubArraySum2 {
+    public static void MaxSubArraySum(int numbers[]) {
+        int currSum = 0;
+        int maxSum = Integer.MIN_VALUE;
+        int prefix[] = new int[numbers.length];
+        // calculation of prefix
+        prefix[0] = numbers[0];
+        for (int i = 1; i < prefix.length; i++) {
+            prefix[i] = prefix[i - 1] + numbers[i];
+        }
+
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = i; j < numbers.length; j++) {
+                currSum = i == 0 ? prefix[j] : prefix[j] - prefix[i - 1];
+                System.out.println(currSum);
+                if (maxSum < currSum) {
+                    maxSum = currSum;
+                }
+            }
+        }
+        System.out.println("Max Sum =" + maxSum);
+    }
+
+    public static void main(String args[]) {
+        int arrays[] = { 1, -2, 6, -1, 3 };
+        MaxSubArraySum(arrays);
+    }
+
+}
